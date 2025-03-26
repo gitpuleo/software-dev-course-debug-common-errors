57e8cdb8be32c746fef2998e3c1055df1d934517
nfp <nfpuleo@gmail.com>
2025-03-25 19:58:26 -0500

    All bugs debugged

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,23 +24,24 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
-// What’s Wrong?
-
+/* What’s Wrong? Answer: A simple syntax error, the pantheses and quotation mark both need to be
+closed and, for good measure and convention, a semicolon appended to the end.*/
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
-// What’s Wrong?
-
+/* What’s Wrong? Answewr: It crashes when it encounters the string "eight" and attempts to double it, 
+which of course it can only do if it is a numbers.
+*/
 
 
 // Program C (Logic Error)
@@ -51,12 +52,14 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
-// What’s Wrong?
+/* What’s Wrong? Answer: Simply that true and false are in the wrong places and need to be swapped.
+The if statement with modulus in it is the one checking whether it is not prime.
+*/
